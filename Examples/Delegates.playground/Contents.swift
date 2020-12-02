2edae32b65080b738991e813714f8ed501527e64
Denis Kovalev <denis.kovalev@saritasa.com>
2020-12-02 12:42:25 +0700

    Update delegate example with more realistic classes.

--- Examples/Delegates.playground/Contents.swift
+++ Examples/Delegates.playground/Contents.swift
@@ -1,117 +1,77 @@
-import Foundation
+import UIKit
+import PlaygroundSupport
 
-// MARK: - Public optional delegate example
+// MARK: - View with delegate example
 
-/// A delegate protocol, which contains events of Actor1 class
-protocol Actor1Delegate: AnyObject {
+/// A delegate protocol, which contains events of ListView class
+protocol ListViewDelegate: AnyObject {
 
     /// Event action without any additional parameters
-    func actor1DidSomething(_ actor: Actor1)
+    func listViewDidTapDone(_ view: ListView)
 
     /// Event action with additional parameters
-    func actor1(_ actor: Actor1, didSomethingWithValue value: Int)
+    func listView(_ view: ListView, didSelectItemAt index: Int)
 }
 
-/// Actor class, which produces some actions
-class Actor1: NSObject {
+/// A class, which produces some actions
+class ListView: UIView {
 
     /// Declare the public delegate
-    weak var delegate: Actor1Delegate?
+    weak var delegate: ListViewDelegate?
 
-    override init() {
-        super.init()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
 
-        // Emit some action in 1 sec
+        // Emit tap action in 1 sec
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
-            self?.doSomething()
+            self?.tapDone()
         }
 
-        // Emit another action in 2 sec
+        // Emit select item action in 2 sec
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { [weak self] in
-            self?.doSomethingWithValue(value: 1)
+            self?.selectItemAt(index: 1)
         }
     }
 
-    private func doSomething() {
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func tapDone() {
         // Call the appropriate delegate method
-        delegate?.actor1DidSomething(self)
+        delegate?.listViewDidTapDone(self)
     }
 
-    private func doSomethingWithValue(value: Int) {
+    private func selectItemAt(index: Int) {
         // Call the appropriate delegate method
-        delegate?.actor1(self, didSomethingWithValue: value)
+        delegate?.listView(self, didSelectItemAt: index)
     }
 }
 
-// MARK: - Private optional delegate example
+// MARK: - Delegate implementation example
 
-/// A delegate protocol, which contains events of Actor2 class
-protocol Actor2Delegate: AnyObject {
-    /// Event action without any additional parameters
-    func actor2DidSomething(_ actor: Actor2)
-}
+class SomeViewController: UIViewController {
+    /// Initialize view
+    private let listView = ListView(frame: .zero)
 
-class Actor2: NSObject {
-
-    /// Declare the private delegate
-    private weak var delegate: Actor2Delegate?
-
-    override init() {
-        super.init()
-
-        // Emit the action in 3 sec
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) { [weak self] in
-            self?.doSomething()
-        }
-    }
-
-    /// Public method with delegate parameter
-    func subscribeOn(delegate: Actor2Delegate) {
-        self.delegate = delegate
-    }
-
-    private func doSomething() {
-        // Call the appropriate delegate method
-        delegate?.actor2DidSomething(self)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Subscribe on view actions by public delegate property
+        listView.delegate = self
     }
 }
 
-// MARK: - Delegate implementation
-
-class Adopter: NSObject {
-
-    /// Initialize actors
-    private let actor1 = Actor1()
-    private let actor2 = Actor2()
-
-    func start() {
-        // Subscribe on actor1 actions by public delegate property
-        actor1.delegate = self
-        // Subscribe on actor2 actions through public method (delegate is still private)
-        actor2.subscribeOn(delegate: self)
+/// Delegate methods implementation for ListView
+extension SomeViewController: ListViewDelegate {
+    func listViewDidTapDone(_ view: ListView) {
+        print("Done tapped!")
     }
 
-}
-
-/// Delegate implementation for Actor1
-extension Adopter: Actor1Delegate {
-    func actor1DidSomething(_ actor: Actor1) {
-        print("Actor1 did something")
-    }
-
-    func actor1(_ actor: Actor1, didSomethingWithValue value: Int) {
-        print("Actor1 did something with value: \(value)")
-    }
-}
-
-/// Delegate implementation for Actor2
-extension Adopter: Actor2Delegate {
-    func actor2DidSomething(_ actor: Actor2) {
-        print("Actor2 did something")
+    func listView(_ view: ListView, didSelectItemAt index: Int) {
+        print("Item selected at: \(index)")
     }
 }
 
 // MARK: - Run
 
-let adopter = Adopter()
-adopter.start()
+PlaygroundPage.current.liveView = SomeViewController()
